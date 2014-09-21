4bff64f4ce771cfb32be7b503f27acec5e1d6160
Mikhail Dyakov <wtltl2@gmail.com>
2014-09-22 00:23:40 +0200

    Try heroku config.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-json "0.3.1"]
                  [ring-cors "0.1.4"]
+                 [ring/ring-servlet "1.3.1"]
                  [compojure "1.1.8"]
                  [clj-http "1.0.0"]
                  [cheshire "5.3.1"]
@@ -14,4 +15,5 @@
   :ring {:handler dict-server.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                        [ring-mock "0.1.5"]]}}
+  :min-lein-version "2.0.0")
